5a4317ef024e767fb9d97e7f65502cde1bb04751
lore-carbonaro <lorenzocarbonaro3@gmail.com>
2025-02-25 14:58:55 +0100

    Write game logic and pseudo-code

---
+++ rps.js
@@ -0,0 +1,33 @@
+/* 
+This game will be played entirely through the console.
+The user will compete against the computer.
+The rules are from rock, paper, scissors: the user will
+input their choice, then the computer will choose one of the 
+three options at random.
+The script evaluates the two inputs and declares which one 
+wins over the other, or if the round results in a tie.
+The script will then increment the user score or 
+computer score by 1.
+When either the computer score or the user score reaches 5, 
+the game ends.
+*/
+
+/* 
+-- Pseudo-code --
+
+- Create a function named getComputerChoice that randomly
+outputs either rock, paper or scissors
+- Create a function named getHumanChoice that asks the user
+to input either rock, paper or scissors in a prompt
+- Initialize two variables, humanScore and computerScore, to 
+keep track of each player's scores (initial value = 0)
+- Create a function named playRound that takes humanChoice 
+and computerChoice as arguments. The function will evaluate 
+which player wins the round by comparing the two choices. If 
+the choices equal each other, the round ends in a tie.
+- Increment humanScore or computerScore by 1 based on the round
+winner
+- Create a function named playGame that loops the playRound
+function until either the humanScore or the computerScore 
+reach 5 points
+*/
